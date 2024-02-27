171f6feb66ddffdfa6306d87b781212487ce8caa
Anna Borodaienko <anna.borodaienko@gmail.com>
2024-02-27 17:13:21 +0200

    feat: add Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Home from './components/Home'
 
 const HomePage = () => {
   return (
-    <div>
-      Home Page
+    <div className='text-9xl text-red-900'>
+      <Home />
     </div>
   )
 }
