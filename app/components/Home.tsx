3530aaed18ff7ee41735b58e47c55d51cd8a9e2c
Anna Borodaienko <anna.borodaienko@gmail.com>
2024-02-28 21:29:32 +0200

    feat: add Reservation section

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -2,6 +2,7 @@ import Delivery from './Delivery/Delivery';
 import Feature from './Features/Feature';
 import Hero from './Hero/Hero';
 import Popular from './Popular/Popular';
+import Reservation from './Reservation/Reservation';
 import Team from './Team/Team';
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
       <Popular />
       <Delivery />
       <Team />
+      <Reservation />
     </div>
   );
 };
